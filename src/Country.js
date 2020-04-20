180f0b95ccde75a234bcb9d2505bebf8723d7f61
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-04-20 13:36:09 +0200

    Country

--- src/Country.js
+++ src/Country.js
@@ -17,22 +17,21 @@ class Country extends Component {
              integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
              crossorigin="anonymous"
                 />
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"></link>
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"></link>
+
                 <div>
                 <header className="headeri1">
                 <img src={this.props.photo} alt="Kosovo flag" className="logoks"/> <br />
                 <ul class="nav justify-content-center">
                             <li className="nav-item">
-                                <a className="nav-link active" href="#"><i class="fas fa-home"></i> Ballina</a>
+                                <a className="nav-link active" href="#"><i className="fa fa-home"></i> Ballina</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#"><i class="fa fa-car"></i> Statistika</a>
+                                <a className="nav-link" href="#"><i className="fa fa-bar-chart"></i> Statistika</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#"> <i class="fas fa-home"></i> Keshilla</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                                <a className="nav-link" href="#"><i className="fas fa-comment-medical"></i>  Keshilla</a>
                             </li>
                             </ul>
                 </header>
@@ -48,7 +47,6 @@ class Country extends Component {
                     <h3 style={{display: "inline"}}>{this.props.data.confirmed}</h3><br />
                     
                     <h3>Recovered: {this.props.data.recovered}</h3>                   
-                    <hr/>
                 </div>
             </div>
          );
