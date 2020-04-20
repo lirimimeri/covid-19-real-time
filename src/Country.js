c25ed4e72ae43d2969d6123632b30e8e59c36b42
lirimimeri <44481883+lirimimeri@users.noreply.github.com>
2020-04-20 14:26:06 +0200

    Update Country.js

--- src/Country.js
+++ src/Country.js
@@ -22,49 +22,12 @@ class Country extends Component {
                 <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"></link>
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"></link>
 
-<<<<<<< HEAD
-                <div>
-                <header className="headeri1">
-                <img src={this.props.photo} alt="Kosovo flag" className="logoks"/> <br />
-                <ul class="nav justify-content-center">
-                            <li className="nav-item">
-                                <a className="nav-link active" href="#"><i className="fa fa-home"></i> Ballina</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#"><i className="fa fa-bar-chart"></i> Statistika</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#"><i className="fas fa-comment-medical"></i>  Keshilla</a>
-                            </li>
-                            </ul>
-                </header>
-                </div>
 
-                
-                
-                <div className="divi1">
-
-                    
-                    
-                    <h5>Last updated: {this.props.data.lastUpdate}</h5>
-
-                    <img src={Death} alt="Deaths" className="Logo"/> 
-                    <h3 style={{display: "inline"}}>{this.props.data.deaths}</h3> <br /> 
-                    
-                    <img src={Infected} alt="infected" className="Logo" /> 
-                    <h3 style={{display: "inline"}}>{this.props.data.confirmed}</h3><br />
-                    
-                    <h3>Recovered: {this.props.data.recovered}</h3>                   
-                </div>
-             
-
-=======
-                
+                                
                 <Header />
                 
                 <CovidData data={this.props.data}/>
                 
->>>>>>> fbbd8c35270170aefa3fbff1969dae433b5ed3fa
             </div>
          );
     }
