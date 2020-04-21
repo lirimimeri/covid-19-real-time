76f38d6d15ef8afaa6b0f7e4a4e41c3c125e18b0
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-04-21 15:52:31 +0200

    testime

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Statistika from '../photos/Chart.jpg'
 
 
 class News extends Component {
 
   render() {
     return <div>
+        {/* <img src={Statistika} alt="Satistika" className="stats"/> FOTO TEST */}
         <h1>Ky text veq per Lantin masi ta prek Ballina</h1>
         <h1>asdasdasd</h1>
         <h1></h1>
