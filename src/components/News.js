98d590fd5a77fe75741527225a5fb37dcb16b1c4
lirimimeri <44481883+lirimimeri@users.noreply.github.com>
2020-04-22 23:59:24 +0200

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -53,32 +53,34 @@ class News extends Component {
             <img src={corona3} width="450px" height="350px" />
           </div>
           <div class="col" style={{textAlign: "left"}}>
-            <h2>ajshjkashdjksahdkjad</h2>
+            <h2>Cilat janë simptomat e një personi të infektuar me koronavirus?</h2>
             <h5>
-              ajdkjahs dkjahskjdajdkj ahsdkjahsk jd jdkjahsdk ahs kjd jdk ahs dk
-              jahskjd ajdkj ahsdkjahskjd ajdkj hsdkj hskjd ajdkj a sdk ahskj
-              dajdk jah kja skjd ajdkjahs dkjahskjdajdkj ahsdkjahsk jd jdkjahsdk
-              ahs kjd jdk ahs dk jahskjd ajdkj ahsdkjahskjd ajdkj hsdkj hskjd
-              ajdkj a sdk ahskj dajdk jah kja skjd ajdkjahs dkjahskjdajdkj
-              ahsdkjahsk jd jdkjahsdk ahs kjd jdk ahs dk jahskjd ajdkj
-              ahsdkjahskjd ajdkj hsdkj hskjd ajdkj a sdk ahskj dajdk jah kja
-              skjd
+            Simptomat varen nga virusi, por shenjat e zakonshme 
+            përfshijnë simptomat e aparatit të frymëmarrjes: ethe, 
+            kollë, gulçim dhe vështirësi në frymëmarrje. Në raste më 
+            të rënda, infeksioni mund të shkaktojë pneumoni, sindromë 
+            të rëndë respiratore akute, insuficiencë të veshkave, 
+            madje edhe vdekje.
+            Kur një sëmundje është e re, nuk ka vaksinë për të, dhe mund të 
+            duhen disa vite derisa të prodhohet një vaksinë e re.
             </h5>
           </div>
         </div>
 
         <div class="row">
           <div class="col" style={{textAlign: "right"}}>
-            <h2>ajshjkashdjksahdkjad</h2>
+            <h2>
+              A mund të infektohen njerëzit me koronavirusin
+              e ri ku si burim infektimi janë kafshët?
+            </h2>
             <h5>
-              ajdkjahs dkjahskjdajdkj ahsdkjahsk jd jdkjahsdk ahs kjd jdk ahs dk
-              jahskjd ajdkj ahsdkjahskjd ajdkj hsdkj hskjd ajdkj a sdk ahskj
-              dajdk jah kja skjd ajdkjahs dkjahskjdajdkj ahsdkjahsk jd jdkjahsdk
-              ahs kjd jdk ahs dk jahskjd ajdkj ahsdkjahskjd ajdkj hsdkj hskjd
-              ajdkj a sdk ahskj dajdk jah kja skjd ajdkjahs dkjahskjdajdkj
-              ahsdkjahsk jd jdkjahsdk ahs kjd jdk ahs dk jahskjd ajdkj
-              ahsdkjahskjd ajdkj hsdkj hskjd ajdkj a sdk ahskj dajdk jah kja
-              skjd
+              Hetimet e hollësishme në Kinë në vitin 2002 kanë
+              zbuluar se SARS-CoV u transmetua nga macet te njerëzit,
+              kurse në Arabinë Saudite në vitin 2012 u zbulua 
+              MERS-CoV nga devetë te njerëzit. Njihen disa koronaviruse 
+              të cilët po qarkullojnë në kafshë, por që akoma nuk kanë 
+              infektuar njerëzit. Ndërkohë që survejanca në të gjithë botën 
+              përmirësohet, ka të ngjarë që të identifikohen më shumë koronaviruse.
             </h5>
           </div>
           <div class="col">
