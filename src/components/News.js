c155225fe43802a1a0fbbd214bd8d22fa441e129
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-04-23 14:02:30 +0200

    update Keshillat

--- src/components/News.js
+++ src/components/News.js
@@ -115,9 +115,28 @@ class News extends Component {
 
           <div className="col">
             <i className="fas fa-house-user fa-3x"></i>
-            <h5>Shmangni daljet e panevojshme!</h5>
+            <h5>Shmangni daljet e panevojshme! Të gjithë po qëndrojmë në shtëpi, edhe ti #rriNshpi !</h5>
           </div>
         </div>
+        <div className="row" style={{ paddingTop: "30px", backgroundColor: "#d3d3d3" }}>
+        <div className="col">
+            <i className="fas fa-people-arrows fa-3x"></i>
+            <h5>
+              {" "}
+              Mbani distancën e nevojshme nga njëri-tjetri!{" "}
+            </h5>
+          </div>
+          <div className="col">
+            <i className="fas fa-handshake-slash fa-3x"></i>
+            <h5> Evitoni dorë-shtrëngimet! Provoni përshëndetjet tjera, është argëtuese!</h5>
+          </div>
+          <div className="col">
+            <i className="fas fa-head-side-cough-slash fa-3x"></i>
+            <h5> Kujdes kur tështini ose kolliteni, mbulojeni gojen me mëngë ose pallomë.</h5>
+          </div>
+          
+        </div>
+          
       </div>
     );
   }
